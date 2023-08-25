3bcab1771c9dfb5cedb00c3bf28cdf7f65557683
Danyl Kecha <kechaofficial@gmail.com>
2023-08-25 17:31:55 +0100

    Added PWA. Small fixes

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -4,11 +4,11 @@ import { auth } from "../config/firebase";
 export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-        return res.status(401).json({ message: "Authorization header is required" });
+        return res.status(401).json({ message: "Authorization is required" });
     }
     const accessToken = authHeader.split(" ")[1];
     if (!accessToken) {
-        return res.status(401).json({ message: "Access token is required" });
+        return res.status(401).json({ message: "Authorization is required" });
     }
 
     try {
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,43 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     envDir: "../.",
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "prompt",
+            includeAssets: [
+                "favicon.ico",
+                "apple-touch-icon.png",
+                "masked-icon.svg",
+            ],
+            manifest: {
+                name: "GoShop: Ecommerce App",
+                short_name: "GoShop",
+                description: "Online retail store for all your needs",
+                theme_color: "#fff",
+                start_url: "/",
+                icons: [
+                    {
+                        src: "pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any maskable",
+                    },
+                ],
+            },
+        }),
+    ],
 });
